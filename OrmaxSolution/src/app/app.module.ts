98b075f99bb416c483be254cd87f896585144e48
vivek dwivedi <vivek1001dwivedi@gmail.com>
2021-09-08 14:53:50 +0530

    adding the components

--- OrmaxSolution/src/app/app.module.ts
+++ OrmaxSolution/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './AboutUs/about-us.component';
+import { FooterComponent } from './Footer/footer/footer.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
