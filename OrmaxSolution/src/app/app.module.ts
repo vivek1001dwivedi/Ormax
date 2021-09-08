b8dc90e4e26ddf0f12094948001d61feefbd5707
vivek dwivedi <vivek1001dwivedi@gmail.com>
2021-09-08 15:42:26 +0530

    fixing issues

--- OrmaxSolution/src/app/app.module.ts
+++ OrmaxSolution/src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './AboutUs/about-us.component';
-import { FooterComponent } from './Footer/footer/footer.component';
-import { HeaderComponent } from './Header/header/header.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent,
-    FooterComponent,
-    HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
